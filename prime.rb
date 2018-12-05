92db25ec722cdd237a78dcd4916a38e09df2156c
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-05 05:13:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+# test every number less than integer
+# to see if it's a factor
+
+#what if we start with integer-1 and move down to 0?
+#not sure why it's returning true for 40 % 4
+
+  if integer <= 1
+    false
+  elsif integer == 2
+    true
+  else
+    (2..integer/2).none? {|i| integer % i == 0}
+  end
+end
